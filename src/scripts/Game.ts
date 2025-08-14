d45da0eebd74eb490bf7932dddd154d48a323ba0
Nydaleclya <github.com.punctual012@passmail.net>
2025-08-14 14:47:16 +0200

    Fixes 2

--- src/scripts/Game.ts
+++ src/scripts/Game.ts
@@ -11,6 +11,7 @@
 class Game implements TmpGameType {
     frameRequest;
     public static achievementCounter = 0;
+    public static loggingCounter = 0;
     public static lastLogDate = 0;
     private _gameState: KnockoutObservable<GameConstants.GameState>;
     private worker: Worker;
@@ -428,6 +429,13 @@ class Game implements TmpGameType {
             Game.achievementCounter = 0;
             AchievementHandler.checkAchievements();
             GameHelper.incrementObservable(App.game.statistics.secondsPlayed);
+        }
+
+        // Log Shinys
+        Game.loggingCounter += GameConstants.TICK_TIME;
+        if (Game.loggingCounter >= 10 * GameConstants.SECOND) {
+            Game.loggingCounter = 0;
+            this.logging();
         }
 
         // Battles
